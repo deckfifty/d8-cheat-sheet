65602cb835e809a7c9ba47cffc77620f1d566c4e
Shane G <superdrupal@gmail.com>
2016-08-11 14:34:50 -0700

    Internal links

--- basic.php
+++ basic.php
@@ -25,3 +25,32 @@ $class = \Drupal\Component\Utility\Html::cleanCssIdentifier($string);
 // Image load & style.
 $image_uri = $entity->field_image->entity->getFileUri();
 $image = \Drupal\image\Entity\ImageStyle::load('large')->buildUrl($image_uri);
+
+// Internal link (fromUri).
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+$options = array(
+  'query'      => ['type' => 'article', 'status' => 1],
+  'fragment'   => 'article-list',
+  'attributes' => ['class' => ['btn', 'btn-mini']],
+  'absolute'   => TRUE,
+);
+$link = Link::fromTextAndUrl(t('Click here'), Url::fromUri('internal:/node', $options))->toString();
+// <a href="http://www.mywebsite.com/node?type=article&status=1#article-list" class="btn btn-mini">Click here</a>
+
+// Internal link (fromRoute).
+use Drupal\Core\Link;
+use Drupal\Core\Url;
+$query = array(
+  'status'   => 1,
+  'type'     => 'All',
+  'title'    => 'article',
+  'langcode' => 'All',
+);
+$options = array(
+  'fragment'   => 'edit-modules-field-types',
+  'attributes' => ['class' => ['btn', 'btn-mini']],
+  'absolute'   => TRUE,
+);
+$link = Link::fromTextAndUrl(t('Click here'), Url::fromRoute('system.admin_content', $query, $options))->toString();
+// <a href="http://www.mywebsite.com/admin/content?status=1&type=article&title=&langcode=All#view-title-table-column" class="btn btn-mini">Click here</a>
